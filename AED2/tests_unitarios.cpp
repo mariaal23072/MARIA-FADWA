b33e56ea99b72297cf76f1c220834ad8fd0d54e1
mariaal23072 <m.almarchalorca@um.es>
2026-03-04 20:36:30 +0100

    yoqse

--- AED2/tests_unitarios.cpp
+++ AED2/tests_unitarios.cpp
@@ -7,7 +7,7 @@ int main () {
     int n = sizeof(A); // actualizo n con la longitud real de la cadena A
     
     // Algoritmo iterativo
-    int pIt;
+    int pIt = 0;
     int maxIt = iterativo(n,m,A,pIt);
 
     cout << "Solucion: B, posicion de inicio igual a " << pIt << ", y numero de caracteres en C igual a " << maxIt << "." << endl;
