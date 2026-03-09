dc4162dd22cb0e538eda2e659dd75a19cbfa31b9
mariaal23072 <m.almarchalorca@um.es>
2026-03-09 14:04:42 +0100

    oraculo mejorado

--- AED2/test_oraculo.cpp
+++ AED2/test_oraculo.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-// Array A: Generar letras de la 'a' a la 'z' en un array
+// Llenar el array con letras aleatorias de la 'a' a la 'z'
 void generarSecuenciaAleatoria(int longitud, char A[], unsigned int semilla) {
     mt19937 gen(semilla);
     uniform_int_distribution<> dist('a', 'z');
@@ -16,31 +16,38 @@ void generarSecuenciaAleatoria(int longitud, char A[], unsigned int semilla) {
 }
 
 int main() {
-    unsigned int semilla = 42;
+
+    unsigned int semilla = 42; // Semilla ej para generar secuencias aleatorias
     int m = 5;
 
+    // Tamaños de prueba
     vector<int> longitudes = {5, 10, 20, 50, 100, 1000};
 
     for (size_t i = 0; i < longitudes.size(); ++i) {
         int longitud = longitudes[i];
+        // Aqui guardamos la secuencia de letras
         char A[longitud];
         
+        // Cambiamos la semilla en cada vuelta para que no salga siempre igual
         generarSecuenciaAleatoria(longitud, A, semilla + i);
 
+        // Posicion que devuelve cada algoritmo
         int pDyv = 0;
         int pIter = 0;
 
-        int dyvOutput = dyv(0, longitud - 1, m, A, pDyv);
+        // Probamos los dos algoritmos: DyV e iterativo
         int resultadoIterativo = iterativo(longitud, m, A, pIter);
+        int dyvOutput = dyv(0, longitud - 1, m, A, pDyv);
         
         cout << "Longitud de la secuencia: " << longitud << endl;
-        cout << "Resultado DyV: " << dyvOutput << " (p=" << pDyv << ")" << endl;
-        cout << "Resultado Iter: " << resultadoIterativo << " (p=" << pIter << ")" << endl;
+        cout << "Resultado DyV: " << dyvOutput << " (p = " << pDyv << ")" << endl;
+        cout << "Resultado Iter: " << resultadoIterativo << " (p = " << pIter << ")" << endl;
         
-        // Compara el máximo y la posición p
+        // Si esto falla, es que los dos metodos no coinciden (compara el resultado y la posicion de ambos algoritmos)
         assert(dyvOutput == resultadoIterativo && pDyv == pIter);
     }
     
-    cout << "Pruebas superadas" << endl;
+    // Si llegamos aqui, todo ha ido bien
+    cout << "Pruebas superadas!!" << endl;
     return 0;
-}
+} 
