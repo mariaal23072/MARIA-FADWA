0ac1766bcaa3c77e62a5b7b86310add03d03f03b
mariaal23072 <m.almarchalorca@um.es>
2026-03-12 00:37:20 +0100

    testuni nuevo

--- AED2/tests_unitarios.cpp
+++ AED2/tests_unitarios.cpp
@@ -1,24 +1,84 @@
 #include <iostream>
+#include <cassert>
 #include "dyv.h"
 
-int main () {
+using namespace std;
 
-    char A[] = {'c', 'd', 'd', 'a', 'b', 'c', 'd', 'a', 'c', 'c', 'a', 'a','a', 'a','a', 'a', 'a' };
-    int n = sizeof(A); // actualizo n con la longitud real de la cadena A
+void testPrincipio() {
+    int n = 8;
+    int m = 4;
+    char A[] = {'a', 'b', 'c', 'd', 'z', 'y', 'x', 'w'};
+    int p = 0;
     
-    // Algoritmo iterativo
-    int pIt = 0;
-    int maxIt = iterativo(n,m,A,pIt);
+    int resultado = dyv(0, n - 1, m, A, p);
+    assert(resultado == 4);
+    assert(p == 1);
+}
 
-    // Algoritmo recursivo dyv
-    int pDyv = 0;
-    int maxDyv = dyv(0, n, m, A, pDyv);
-
-    // Iterativo:
-    cout << "Iterativo:\nSolucion: B, posicion de inicio igual a " << pIt << ", y numero de caracteres en C igual a " << maxIt << "." << endl;
+void testFinal() {
+    int n = 8;
+    int m = 4;
+    char A[] = {'z', 'x', 'x', 'w', 'a', 'b', 'c', 'a'};
+    int p = 0;
     
-    // DyV:
-    cout << "Divide y Venceras:\nSolucion: B, posicion de inicio igual a " << pDyv << ", y numero de caracteres en C igual a " << maxDyv << "." << endl;
+    int resultado = dyv(0, n - 1, m, A, p);
+    assert(resultado == 3);
+    assert(p == 4 );
+}
 
+void testMitad() {
+    int n = 8;
+    int m = 4;
+    char A[] = {'z', 'y', 'a', 'b', 'c', 'd', 'x', 'w'};
+    int p = 0;
+    
+    int resultado = dyv(0, n - 1, m, A, p);
+    assert(resultado == 4);
+    assert(p == 3);
+}
+
+void testMejorConstante() {
+    int n = 8;
+    int m = 4;
+    char A[] = {'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'};
+    int p = 0;
+    
+    int resultado = dyv(0, n - 1, m, A, p);
+    assert(resultado == 4);
+    assert(p == 1);
+}
+
+void testMejorCreciente() {
+    int n = 8;
+    int m = 4;
+    char A[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+    int p = 0;
+    
+    int resultado = dyv(0, n - 1, m, A, p);
+    assert(resultado == 4);
+    assert(p == 1);
+}
+
+void testPeorDecreciente() {
+    int n = 8;
+    int m = 4;
+    char A[] = {'z', 'y', 'x', 'w', 'v', 'u', 't', 's'};
+    int p = 0;
+    
+    int resultado = dyv(0, n - 1, m, A, p);
+    assert(resultado == 1);
+    assert(p == 1);
+}
+
+int main() {
+    
+    testPrincipio();
+    testFinal();
+    testMitad();
+    testMejorConstante();
+    testMejorCreciente();
+    testPeorDecreciente();
+    
+    cout << "Todos los tests pasaron correctamente." << endl;
     return 0;
 }
