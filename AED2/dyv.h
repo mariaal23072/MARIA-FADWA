433991bc73721bc6fa69b52e43160c018c6c37e0
mariaal23072 <m.almarchalorca@um.es>
2026-03-12 12:09:42 +0100

    todo

--- AED2/dyv.h
+++ AED2/dyv.h
@@ -5,7 +5,7 @@
 using namespace std;
 
 // m<=n, donde n = longitud de la cadena A, y m = longitud de la cadena B
-const int m = 100; // longitud de la cadena B
+const int m = 5; // longitud de la cadena B
 
  // Implementación del algoritmo iterativo
 int iterativo(int n, int m, char A[], int &p);
