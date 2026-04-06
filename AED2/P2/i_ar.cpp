f9e03016ebfbf6c3cfe30e90ce1c89d64a1b19a5
mariaal23072 <m.almarchalorca@um.es>
2026-04-06 21:16:20 +0200

    I_AR

---
+++ AED2/P2/i_ar.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+using namespace std;
+
+const int MAX_ALUMNOS = 100;
+const int MAX_PAREJAS = (MAX_ALUMNOS * (MAX_ALUMNOS - 1)) / 2;
+
+struct Pupitre {
+    int alum1;
+    int alum2;
+    int beneficio;
+};
+
+int organizacionClase(int N, int amistad[MAX_ALUMNOS][MAX_ALUMNOS], int trabajo[MAX_ALUMNOS][MAX_ALUMNOS], int solucion[MAX_ALUMNOS]) {
+    
+    int beneficio_total = 0;
+    int pareja = 0;
+    bool emparejado[MAX_ALUMNOS] = {};
+    Pupitre pupitre[MAX_PAREJAS];
+
+    for (int i = 0; i < N; i++) {
+        for (int j = i+1; j < N; j++) { // i+1 para no generar parejas repetidas
+            pupitre[pareja].beneficio = (amistad[i][j] + amistad[j][i]) * (trabajo[i][j] + trabajo[j][i]);
+            pupitre[pareja].alum1 = i;
+            pupitre[pareja].alum2 = j;
+
+            pareja++;
+
+        }
+    }
+
+    // Ordenar parejas por beneficio de mayor a menor
+    for (int i = 0; i < pareja - 1; i++) {
+        for (int j = 0; j < pareja - i - 1; j++) {
+            if (pupitre[j].beneficio < pupitre[j+1].beneficio) {
+                Pupitre temp = pupitre[j];
+                pupitre[j] = pupitre[j+1];
+                pupitre[j+1] = temp;
+            }
+        }
+    }
+
+    int indice = 0;
+    // elegir parejas válidas dentro de las ordenadas
+    for (int k = 0; k < pareja; k++) {
+        if (!emparejado[pupitre[k].alum1] && !emparejado[pupitre[k].alum2]) {
+            emparejado[pupitre[k].alum1] = true;
+            emparejado[pupitre[k].alum2] = true;
+            beneficio_total = beneficio_total + pupitre[k].beneficio;
+            
+            solucion[indice] = pupitre[k].alum1;
+            indice++;
+            solucion[indice] = pupitre[k].alum2;
+            indice++;
+        }
+    }
+
+    // En caso de que haya alumnos impares
+    if (N % 2 != 0) {
+        for (int i = 0; i < N; i++) {
+            if (!emparejado[i]) {
+                solucion[indice] = i;
+            }
+        }
+    }
+
+
+
+
+    return beneficio_total;
+}
+
+int main () {
+
+    // ENTRADA
+    int P; // número de casos prueba
+    cin >> P;
+
+    // Bucle para cada caso prueba
+    for (int i = 0; i < P; i++) {
+        int N; // número de alumnos
+        cin >> N;
+
+        int amistad[MAX_ALUMNOS][MAX_ALUMNOS] = {};
+        int trabajo[MAX_ALUMNOS][MAX_ALUMNOS] = {};
+
+        // Bucle para matriz amistad
+        for (int fila = 0; fila < N; fila++) {
+            for (int columna = 0; columna < N-1; columna++) {
+                if (columna < fila){
+                    cin >> amistad[fila][columna]; // antes de la diagonal
+                }
+                else {
+                    cin >> amistad[fila][columna+1]; // saltar la diagonal
+                }
+            }
+        }
+
+        // Bucle apra matriz de trabajo
+        for (int fila = 0; fila < N; fila++) {
+            for (int columna = 0; columna < N-1; columna++) {
+                if (columna < fila){
+                    cin >> trabajo[fila][columna];
+                }
+                else {
+                    cin >> trabajo[fila][columna+1];
+                }
+            }
+        }
+
+        // SALIDA
+        int solucion[MAX_ALUMNOS] = {};
+        int beneficio_total = organizacionClase(N, amistad, trabajo, solucion);
+        
+        cout << beneficio_total << endl;
+        for (int alumn = 0; alumn < N; alumn++) {
+            cout << solucion[alumn] << " ";
+        }
+        cout << endl;
+  
+
+    }
+
+    return 0;
+}
